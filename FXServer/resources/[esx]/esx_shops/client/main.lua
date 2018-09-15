a07041f05398b33c1d88324adbffd80750b8f45c
Nexusrisen <bdkohn91@gmail.com>
2018-09-15 16:15:11 -0400

    Updated Shops

--- FXServer/resources/[esx]/esx_shops/client/main.lua
+++ FXServer/resources/[esx]/esx_shops/client/main.lua
@@ -4,101 +4,121 @@ local LastZone                = nil
 local CurrentAction           = nil
 local CurrentActionMsg        = ''
 local CurrentActionData       = {}
+local PlayerData              = {}
 
 Citizen.CreateThread(function()
 	while ESX == nil do
 		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 		Citizen.Wait(0)
 	end
-end)
 
-AddEventHandler('onClientMapStart', function()
+	Citizen.Wait(5000)
+	PlayerData = ESX.GetPlayerData()
 
-	ESX.TriggerServerCallback('esx_shop:requestDBItems', function(ShopItems)
+	ESX.TriggerServerCallback('esx_shops:requestDBItems', function(ShopItems)
 		for k,v in pairs(ShopItems) do
 			Config.Zones[k].Items = v
 		end
 	end)
-
 end)
 
 function OpenShopMenu(zone)
+	PlayerData = ESX.GetPlayerData()
+
 	local elements = {}
 	for i=1, #Config.Zones[zone].Items, 1 do
-
 		local item = Config.Zones[zone].Items[i]
+
+		if item.limit == -1 then
+			item.limit = 100
+		end
+
 		table.insert(elements, {
-			label     = item.label .. ' - <span style="color: green;">' .. item.price .. '$</span>',
-			realLabel = item.label,
-			value     = item.name,
-			price     = item.price
+			label      = item.label .. ' - <span style="color: green;">$' .. item.price .. '</span>',
+			label_real = item.label,
+			item       = item.item,
+			price      = item.price,
+
+			-- menu properties
+			value      = 1,
+			type       = 'slider',
+			min        = 1,
+			max        = item.limit
 		})
 	end
 
 	ESX.UI.Menu.CloseAll()
-	ESX.UI.Menu.Open(
-		'default', GetCurrentResourceName(), 'shop',
-		{
-			title    = _U('shop'),
+	ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'shop', {
+		title    = _U('shop'),
+		align    = 'bottom-right',
+		elements = elements
+	}, function(data, menu)
+		ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'shop_confirm', {
+			title    = _U('shop_confirm', data.current.value, data.current.label_real, data.current.price * data.current.value),
 			align    = 'bottom-right',
-			elements = elements
-		},
-		function(data, menu)
-			TriggerServerEvent('esx_shop:buyItem', data.current.value, data.current.price)
-		end,
-		function(data, menu)
-			menu.close()
-			CurrentAction     = 'shop_menu'
-			CurrentActionMsg  = _U('press_menu')
-			CurrentActionData = {zone = zone}
-		end
-	)
+			elements = {
+				{label = _U('no'),  value = 'no'},
+				{label = _U('yes'), value = 'yes'}
+			}
+		}, function(data2, menu2)
+			if data2.current.value == 'yes' then
+				TriggerServerEvent('esx_shops:buyItem', data.current.item, data.current.value, zone)
+			end
+
+			menu2.close()
+		end, function(data2, menu2)
+			menu2.close()
+		end)
+	end, function(data, menu)
+		menu.close()
+		CurrentAction     = 'shop_menu'
+		CurrentActionMsg  = _U('press_menu')
+		CurrentActionData = {zone = zone}
+	end)
 end
 
-AddEventHandler('esx_shop:hasEnteredMarker', function(zone)
-
+AddEventHandler('esx_shops:hasEnteredMarker', function(zone)
 	CurrentAction     = 'shop_menu'
 	CurrentActionMsg  = _U('press_menu')
 	CurrentActionData = {zone = zone}
-
 end)
 
-AddEventHandler('esx_shop:hasExitedMarker', function(zone)
-
+AddEventHandler('esx_shops:hasExitedMarker', function(zone)
 	CurrentAction = nil
 	ESX.UI.Menu.CloseAll()
-
 end)
 
 -- Create Blips
 Citizen.CreateThread(function()
 	for k,v in pairs(Config.Zones) do
-  	for i = 1, #v.Pos, 1 do
-		local blip = AddBlipForCoord(v.Pos[i].x, v.Pos[i].y, v.Pos[i].z)
-		SetBlipSprite (blip, 52)
-		SetBlipDisplay(blip, 4)
-		SetBlipScale  (blip, 0.1)
-		SetBlipColour (blip, 2)
-		SetBlipAsShortRange(blip, true)
-		BeginTextCommandSetBlipName("STRING")
-		EndTextCommandSetBlipName(blip)
+		for i = 1, #v.Pos, 1 do
+			local blip = AddBlipForCoord(v.Pos[i].x, v.Pos[i].y, v.Pos[i].z)
+			SetBlipSprite (blip, 52)
+			SetBlipDisplay(blip, 4)
+			SetBlipScale  (blip, 1.0)
+			SetBlipColour (blip, 2)
+			SetBlipAsShortRange(blip, true)
+			BeginTextCommandSetBlipName("STRING")
+			AddTextComponentString(_U('shops'))
+			EndTextCommandSetBlipName(blip)
 		end
 	end
 end)
 
 -- Display markers
 Citizen.CreateThread(function()
-  while true do
-    Citizen.Wait(10)
-    local coords = GetEntityCoords(GetPlayerPed(-1))
-    for k,v in pairs(Config.Zones) do
-      for i = 1, #v.Pos, 1 do
-        if(Config.Type ~= -1 and GetDistanceBetweenCoords(coords, v.Pos[i].x, v.Pos[i].y, v.Pos[i].z, true) < Config.DrawDistance) then
-          DrawMarker(Config.Type, v.Pos[i].x, v.Pos[i].y, v.Pos[i].z, 0.0, 0.0, 0.0, 0, 0.0, 0.0, Config.Size.x, Config.Size.y, Config.Size.z, Config.Color.r, Config.Color.g, Config.Color.b, 100, false, true, 2, false, false, false, false)
-        end
-      end
-    end
-  end
+	while true do
+		Citizen.Wait(10)
+		local coords = GetEntityCoords(GetPlayerPed(-1))
+
+		for k,v in pairs(Config.Zones) do
+			for i = 1, #v.Pos, 1 do
+				if(Config.Type ~= -1 and GetDistanceBetweenCoords(coords, v.Pos[i].x, v.Pos[i].y, v.Pos[i].z, true) < Config.DrawDistance) then
+					DrawMarker(Config.Type, v.Pos[i].x, v.Pos[i].y, v.Pos[i].z, 0.0, 0.0, 0.0, 0, 0.0, 0.0, Config.Size.x, Config.Size.y, Config.Size.z, Config.Color.r, Config.Color.g, Config.Color.b, 100, false, true, 2, false, false, false, false)
+				end
+			end
+		end
+	end
 end)
 
 -- Enter / Exit marker events
@@ -121,35 +141,38 @@ Citizen.CreateThread(function()
 		end
 		if isInMarker and not HasAlreadyEnteredMarker then
 			HasAlreadyEnteredMarker = true
-			TriggerEvent('esx_shop:hasEnteredMarker', currentZone)
+			TriggerEvent('esx_shops:hasEnteredMarker', currentZone)
 		end
 		if not isInMarker and HasAlreadyEnteredMarker then
 			HasAlreadyEnteredMarker = false
-			TriggerEvent('esx_shop:hasExitedMarker', LastZone)
+			TriggerEvent('esx_shops:hasExitedMarker', LastZone)
 		end
 	end
 end)
 
 -- Key Controls
 Citizen.CreateThread(function()
-  while true do
-    Citizen.Wait(10)
-    if CurrentAction ~= nil then
+	while true do
+		Citizen.Wait(10)
 
-      SetTextComponentFormat('STRING')
-      AddTextComponentString(CurrentActionMsg)
-      DisplayHelpTextFromStringLabel(0, 0, 1, -1)
+		if CurrentAction ~= nil then
 
-      if IsControlJustReleased(0, 38) then
+			SetTextComponentFormat('STRING')
+			AddTextComponentString(CurrentActionMsg)
+			DisplayHelpTextFromStringLabel(0, 0, 1, -1)
 
-        if CurrentAction == 'shop_menu' then
-          OpenShopMenu(CurrentActionData.zone)
-        end
+			if IsControlJustReleased(0, 38) then
 
-        CurrentAction = nil
+				if CurrentAction == 'shop_menu' then
+					OpenShopMenu(CurrentActionData.zone)
+				end
 
-      end
+				CurrentAction = nil
 
-    end
-  end
+			end
+
+		else
+			Citizen.Wait(500)
+		end
+	end
 end)
