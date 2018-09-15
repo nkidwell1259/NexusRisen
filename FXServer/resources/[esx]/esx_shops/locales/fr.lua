20693aaa84a841e388f0911fdefed44451f1070a
Nexusrisen <bdkohn91@gmail.com>
2018-09-14 19:10:36 -0700

    Updated Shops - 100%

--- FXServer/resources/[esx]/esx_shops/locales/fr.lua
+++ FXServer/resources/[esx]/esx_shops/locales/fr.lua
@@ -1,11 +1,9 @@
 Locales['fr'] = {
+
 	['shop'] = 'magasin',
 	['shops'] = 'magasins',
 	['press_menu'] = 'appuyez sur ~INPUT_CONTEXT~ pour accéder au magasin.',
-	['bought'] = 'you just bought ~y~%sx~s~ ~b~%s~s~ for ~r~$%s~s~',
-	['not_enough'] = 'vous n\'avez ~r~pas assez~s~ d\'argent: %s',
-	['player_cannot_hold'] = 'you do ~r~not~s~ have enough ~y~free space~s~ in your inventory!',
-	['shop_confirm'] = 'buy %sx %s for $%s?',
-	['no'] = 'no',
-	['yes'] = 'yes',
+	['bought'] = 'vous avez acheté ~b~1x ',
+	['not_enough'] = 'vous n\'avez ~r~pas assez~s~ d\'argent.',
+
 }
